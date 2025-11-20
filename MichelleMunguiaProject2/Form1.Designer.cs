f3c2f5c37ce5a245f1721feab84f7e505eb9facf
M <munguiamichelle03@gmail.com>
2025-11-20 10:06:04 -0500

    GUI Layout

--- MichelleMunguiaProject2/Form1.Designer.cs
+++ MichelleMunguiaProject2/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // 
             // buttonEnter
             // 
-            buttonEnter.Location = new Point(596, 324);
+            buttonEnter.Location = new Point(577, 323);
             buttonEnter.Name = "buttonEnter";
             buttonEnter.Size = new Size(75, 23);
             buttonEnter.TabIndex = 0;
@@ -52,7 +52,7 @@
             // 
             // BoxInput
             // 
-            BoxInput.Location = new Point(346, 325);
+            BoxInput.Location = new Point(327, 324);
             BoxInput.Name = "BoxInput";
             BoxInput.Size = new Size(208, 23);
             BoxInput.TabIndex = 2;
@@ -70,7 +70,7 @@
             // 
             labelTitle.AutoSize = true;
             labelTitle.Font = new Font("Comic Sans MS", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            labelTitle.Location = new Point(346, 50);
+            labelTitle.Location = new Point(327, 25);
             labelTitle.Name = "labelTitle";
             labelTitle.Size = new Size(317, 45);
             labelTitle.TabIndex = 4;
@@ -82,7 +82,7 @@
             label1.BorderStyle = BorderStyle.FixedSingle;
             label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold);
             label1.ForeColor = SystemColors.Control;
-            label1.Location = new Point(334, 256);
+            label1.Location = new Point(315, 255);
             label1.Margin = new Padding(10);
             label1.Name = "label1";
             label1.Size = new Size(35, 42);
@@ -95,7 +95,7 @@
             label2.BorderStyle = BorderStyle.FixedSingle;
             label2.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold);
             label2.ForeColor = SystemColors.Control;
-            label2.Location = new Point(386, 256);
+            label2.Location = new Point(367, 255);
             label2.Margin = new Padding(10);
             label2.Name = "label2";
             label2.Size = new Size(35, 42);
@@ -108,7 +108,7 @@
             label3.BorderStyle = BorderStyle.FixedSingle;
             label3.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold);
             label3.ForeColor = SystemColors.Control;
-            label3.Location = new Point(438, 256);
+            label3.Location = new Point(419, 255);
             label3.Margin = new Padding(10);
             label3.Name = "label3";
             label3.Size = new Size(35, 42);
@@ -121,7 +121,7 @@
             label4.BorderStyle = BorderStyle.FixedSingle;
             label4.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold);
             label4.ForeColor = SystemColors.Control;
-            label4.Location = new Point(490, 256);
+            label4.Location = new Point(471, 255);
             label4.Margin = new Padding(10);
             label4.Name = "label4";
             label4.Size = new Size(35, 42);
@@ -134,7 +134,7 @@
             label5.BorderStyle = BorderStyle.FixedSingle;
             label5.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold);
             label5.ForeColor = SystemColors.Control;
-            label5.Location = new Point(542, 256);
+            label5.Location = new Point(523, 255);
             label5.Margin = new Padding(10);
             label5.Name = "label5";
             label5.Size = new Size(35, 42);
@@ -147,7 +147,7 @@
             label6.BorderStyle = BorderStyle.FixedSingle;
             label6.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold);
             label6.ForeColor = SystemColors.Control;
-            label6.Location = new Point(594, 256);
+            label6.Location = new Point(575, 255);
             label6.Margin = new Padding(10);
             label6.Name = "label6";
             label6.Size = new Size(35, 42);
@@ -160,7 +160,7 @@
             label7.BorderStyle = BorderStyle.FixedSingle;
             label7.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold);
             label7.ForeColor = SystemColors.Control;
-            label7.Location = new Point(646, 256);
+            label7.Location = new Point(627, 255);
             label7.Margin = new Padding(10);
             label7.Name = "label7";
             label7.Size = new Size(35, 42);
@@ -171,7 +171,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(824, 492);
+            ClientSize = new Size(764, 420);
             Controls.Add(label7);
             Controls.Add(label6);
             Controls.Add(label5);
